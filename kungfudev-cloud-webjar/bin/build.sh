25f58ff8829da5153ba80915f1d2357c248fcf95
Kevin Sewell <kevin.w.sewell@gmail.com>
2015-06-13 10:11:40 +0200

    Splitting kungfudev-cloud-webjar into seperate repo

--- kungfudev-cloud-webjar/bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname $0)
-. ${BASEDIR}/../../scripts/build-run.sh
-
-build ${BASEDIR} com.kungfudev.cloud.webjar.WebJarApplication
--- kungfudev-cloud-webjar/bin/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname $0)
-. ${BASEDIR}/../../scripts/build-run.sh
-
-run ${BASEDIR} com.kungfudev.cloud.webjar.WebJarApplication
